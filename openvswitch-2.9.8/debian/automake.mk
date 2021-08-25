002d531a11d8620ec7dc03749e54cc33d2f2e1a9
Jeff Squyres <jsquyres@cisco.com>
2021-08-25 14:50:05 -0700

    Initial import of openvswitch-switch Ubuntu package sources
    Imported from Ubuntu 18.04 openvswitch-switch, package version
    2.9.8-0ubuntu0.18.04.2.
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

---
+++ openvswitch-2.9.8/debian/automake.mk
@@ -0,0 +1,70 @@
+EXTRA_DIST += \
+	debian/changelog \
+	debian/compat \
+	debian/control \
+	debian/copyright \
+	debian/copyright.in \
+	debian/dirs \
+	debian/openvswitch-common.dirs \
+	debian/openvswitch-common.install \
+	debian/openvswitch-pki.dirs \
+	debian/openvswitch-pki.postinst \
+	debian/openvswitch-pki.postrm \
+	debian/openvswitch-switch.README.Debian \
+	debian/openvswitch-switch.dirs \
+	debian/openvswitch-switch.init \
+	debian/openvswitch-switch.install \
+	debian/openvswitch-switch.logrotate \
+	debian/openvswitch-switch.postinst \
+	debian/openvswitch-switch.postrm \
+	debian/openvswitch-switch.default \
+	debian/openvswitch-switch.links \
+	debian/openvswitch-test.install \
+	debian/openvswitch-testcontroller.README.Debian \
+	debian/openvswitch-testcontroller.default \
+	debian/openvswitch-testcontroller.dirs \
+	debian/openvswitch-testcontroller.init \
+	debian/openvswitch-testcontroller.install \
+	debian/openvswitch-testcontroller.postinst \
+	debian/openvswitch-testcontroller.postrm \
+	debian/openvswitch-vtep.default \
+	debian/openvswitch-vtep.dirs \
+	debian/openvswitch-vtep.init \
+	debian/openvswitch-vtep.install \
+	debian/ovn-central.init \
+	debian/ovn-central.install \
+	debian/ovn-central.postrm \
+	debian/ovn-central.default \
+	debian/ovn-common.install \
+	debian/ovn-common.postinst \
+	debian/ovn-common.postrm \
+    debian/ovn-docker.install \
+	debian/ovn-host.init \
+	debian/ovn-host.install \
+	debian/ovn-host.postrm \
+	debian/ovn-host.default \
+	debian/rules \
+	debian/ifupdown.sh \
+	debian/source/format
+
+check-debian-changelog-version:
+	@DEB_VERSION=`echo '$(VERSION)' | sed 's/pre/~pre/'`;		     \
+	if $(FGREP) '($(DEB_VERSION)' $(srcdir)/debian/changelog >/dev/null; \
+	then								     \
+	  :;								     \
+	else								     \
+	  echo "Update debian/changelog to mention version $(VERSION)";	     \
+	  exit 1;							     \
+	fi
+ALL_LOCAL += check-debian-changelog-version
+DIST_HOOKS += check-debian-changelog-version
+
+$(srcdir)/debian/copyright: AUTHORS.rst debian/copyright.in
+	$(AM_V_GEN) \
+	{ sed -n -e '/%AUTHORS%/q' -e p < $(srcdir)/debian/copyright.in;   \
+	  sed '34,/^$$/d' $(srcdir)/AUTHORS.rst  |				   \
+		sed -n -e '/^$$/q' -e 's/^/  /p';			   \
+	  sed -e '34,/%AUTHORS%/d' $(srcdir)/debian/copyright.in;	   \
+	} > $@
+
+DISTCLEANFILES += debian/copyright
